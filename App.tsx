0a4e27b8aa8a2a9d11641f16404a569c28db65e5
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-08-21 15:06:53 +0300

    feat: minor changes

--- App.tsx
+++ App.tsx
@@ -1,28 +1,64 @@
-import {StyleSheet, TextInput, View} from 'react-native';
-import {useState} from 'react';
+import {StyleSheet, TextInput, View, Text, TouchableWithoutFeedback, Keyboard} from 'react-native';
+import React, {ReactNode, useState} from 'react';
+import {Checkbox} from 'expo-checkbox';
 
 export default function App() {
     const [value, setValue] = useState('');
 
+    const [tasks, setTasks] = useState([
+        {id: 1, title: 'HTML', isDone: true},
+        {id: 2, title: 'CSS', isDone: true},
+        {id: 3, title: 'JS', isDone: false},
+        {id: 4, title: 'React', isDone: true},
+        {id: 5, title: 'React Native', isDone: false},
+    ]);
+
     return (
         <View style={styles.container}>
-            <TextInput value={value} onChangeText={setValue} style={[globalStyles.border, styles.input]}/>
+            <HideKeyboard>
+                <View style={[globalStyles.border, {width: '80%'}]}>
+                    <TextInput value={value} onChangeText={setValue} style={styles.input}/>
+                </View>
+            </HideKeyboard>
+            <View style={{width: '60%'}}>
+                {tasks.map(task => (
+                    <View key={task.id} style={[globalStyles.border, styles.boxTask]}>
+                        <Checkbox value={task.isDone}></Checkbox>
+                        <Text>{task.title}</Text>
+                    </View>
+                ))}
+            </View>
         </View>
     );
 }
 
+const HideKeyboard = ({children}: { children: ReactNode }) => (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        {children}
+    </TouchableWithoutFeedback>
+);
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#4e8d4f',
+        backgroundColor: '#0f0e17',
         alignItems: 'center',
         justifyContent: 'center',
     },
     input: {
-        padding: 10,
-        width: 200,
+        marginBottom: 15,
+        padding: 4,
+        // width: '60%',
         backgroundColor: '#fff',
         fontSize: 18,
+    },
+    boxTask: {
+        flexDirection: 'row',
+        marginVertical: 4,
+        paddingVertical: 4,
+        paddingHorizontal: 10,
+        backgroundColor: '#fffffe',
+        justifyContent: 'space-between',
     },
 });
 
@@ -31,5 +67,5 @@ const globalStyles = StyleSheet.create({
         borderStyle: 'solid',
         borderColor: 'red',
         borderWidth: 1,
-    }
+    },
 });
