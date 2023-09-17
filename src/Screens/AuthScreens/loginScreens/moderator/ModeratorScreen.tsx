0dcea172fcb18942da249c462dcc5d70cc104895
Volodymyr Yaremchak <vovkahorror@gmail.com>
2023-09-17 12:20:29 +0300

    feat: added Moderator screen

---
+++ src/Screens/AuthScreens/loginScreens/moderator/ModeratorScreen.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from 'react-native';
+
+export const ModeratorScreen = () => {
+    return (
+        <View>
+            <Text>Moderator screen</Text>
+        </View>
+    );
+};
